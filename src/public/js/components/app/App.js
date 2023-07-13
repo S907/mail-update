5397000c37a0d30b06883ec94a61fdcced31375e
s907 <souvikghosh146@gmail.com>
2023-07-13 20:14:09 +0530

    webpack issues

---
+++ src/public/js/components/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../header/Header'
+const App = () => {
+    console.log(window);
+  return (
+    <div>
+    <Header />
+    </div>
+  )
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports={
     module:{
         rules:[
             {
-                test:/\.jsx?/,
+                test:/\.js$/,
                 exclude:/node_modules/,
                 laoder:"babel-loader",
                 query:{
